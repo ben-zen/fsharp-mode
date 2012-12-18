fa9855499c7afefd65b5c473b9fde5b9bbceb58e
Dario Bertini <berdario@gmail.com>
2012-12-18 14:37:21 +0100

    Emacs: Changed fsharp to fsharp-mode for consistency with other emacs packages

--- monodevelop/install-release.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set MSBUILD=%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe
-%MSBUILD% ..\FSharp.CompilerBinding\FSharp.CompilerBinding.fsproj /p:Configuration=Release
-%MSBUILD% MonoDevelop.FSharpBinding\MonoDevelop.FSharp.windows.fsproj /p:Configuration=Release
-set MDROOT="%ProgramFiles(x86)%\MonoDevelop"
-rmdir /s /q pack
-mkdir pack\windows\Release
-%MDROOT%\bin\mdtool.exe setup pack bin\windows\Release\FSharpBinding.windows.addin.xml -d:pack\windows\Release
-%MDROOT%\bin\mdtool.exe setup install -y pack\windows\Release\MonoDevelop.FSharpBinding_3.2.12.mpack 
